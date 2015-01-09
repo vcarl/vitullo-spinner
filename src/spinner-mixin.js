6b23efa550a12e3007dcb2af3752973c1c02868b
Carl Vitullo <carl.vitullo@gmail.com>
2015-01-09 14:32:44 -0500

    Unit tests for spinner-mixin

--- src/spinner-mixin.js
+++ src/spinner-mixin.js
@@ -1,4 +1,5 @@
 (function() {
+	'use strict';
 	// SpinnerMixin is a mixin for React components. It allows you to add an 
 	// abitrary number of spinners that will display instead of the DOM while 
 	// waiting on an async task (network, etc).
@@ -7,17 +8,16 @@
 		// or an array of strings can be passed. They're accessible by the string 
 		// name given.
 		addSpinners: function(name) {
+			var state;
+			state = {};
 			if (typeof name === "string") {
-				var state = {};
 				state[name] = true;
-				this.setState({ spinners: state });
 			} else if (Array.isArray(name)) {
-				var state = {};
 				name.forEach(function(val) {
 					state[val] = true;
 				});
-				this.setState({ spinners: state });
 			}
+			this.setState({ spinners: state });
 		},
 		// getSpinner takes a string spinner name and fetches its value.
 		getSpinner: function(spinner) {
@@ -36,7 +36,7 @@
 		},
 		stopSpinner: function(spinner) {
 			if (typeof this.state.spinners[spinner] === 'undefined') {
-				throw new Error("Spinner \"" + spinner + "\" does not exist.");
+				throw new Error("Can't stop spinner \"" + spinner + "\", does not exist.");
 			}
 			var state = this.state.spinners;
 			state[spinner] = true;
